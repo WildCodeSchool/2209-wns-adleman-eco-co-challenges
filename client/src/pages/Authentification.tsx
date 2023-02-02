36c764d15f666326b891fbaab1f738b916c8b79d
Marc Hazime <mhazime@dydu.ai>
2023-02-02 15:59:16 +0100

    new color and logo on auth

--- client/src/pages/Authentification.tsx
+++ client/src/pages/Authentification.tsx
@@ -1,7 +1,13 @@
-import AuthForm from '../components/AuthForm/AuthForm'
+import AuthForm from "../components/AuthForm/AuthForm";
+import Logo from "../components/Logo/Logo";
 
-export default function Authentification()  {
-return (
-    <AuthForm />
-)
+export default function Authentification() {
+  return (
+    <div id="login-page" className="">
+      <div id="logo" className="">
+        <Logo />
+      </div>
+      <AuthForm />
+    </div>
+  );
 }
