2e52a6532193e0a282379b8cb36914f69bc9cfb0
Antoine Dumez <antoine.dumez@soan-solutions.io>
2023-04-03 16:40:11 +0200

    modify deploy staging script

--- bin/sh/deploy-staging.sh
+++ bin/sh/deploy-staging.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-chmod +x /home/wns_student/apps/ecocochallenges/production/bin/sh/deploy-staging.sh && \
-cd .. && cd .. && git fetch origin && git reset --hard origin/main && git clean -f -d && \
-docker compose -f docker-compose.staging.yml down && \
-docker compose -f docker-compose.staging.yml pull && \
-docker compose -f docker-compose.staging.yml --env-file .env.staging up -d;
+#chmod +x /home/wns_student/apps/ecocochallenges/production/bin/sh/deploy-staging.sh && \
+#cd .. && cd .. && git fetch origin && git reset --hard origin/main && git clean -f -d && \
+#docker compose -f docker-compose.staging.yml down && \
+#docker compose -f docker-compose.staging.yml pull && \
+#docker compose -f docker-compose.staging.yml --env-file .env.staging up -d;
+
+cd ../.. && git fetch origin && git reset --hard origin/develop clean -f -d && \
+docker-compose up --no-recreate --no-build --force-recreate -d;
