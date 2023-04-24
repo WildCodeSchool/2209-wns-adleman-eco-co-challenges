a2aafd157fe84a1071cf2e86a1ef74bc0b5b9fa1
Antoine <dumez.a@outlook.com>
2023-04-24 15:58:53 +0200

    feat: add the database reboot before each test and few fix

--- tests/src/user.test.ts
+++ tests/src/user.test.ts
@@ -13,7 +13,6 @@ const client = new ApolloClient({
   }),
   cache: new InMemoryCache(),
 });
-
 const createUserMutation = gql`
   mutation CreateUser($data: UserInput!) {
   createUser(data: $data) {
@@ -22,22 +21,14 @@ const createUserMutation = gql`
 }
 `;
 
-
-// const readUsersQuery = gql`
-//   query Users {
-//     users {
-//       nickName
-//     }
-//   }
-// `;
-
+//TODO: add real CRUD tests for each entities
 describe("user resolver", () => {
   describe("create user", () => {
     it("should create user given valid attributes", async () => {
       const res = await client.mutate({
         mutation: createUserMutation,
         variables: { data: {
-          nickName: "efzefezfzef",
+          nickName: "jetest",
           password: "@zazaIHiozaoec55"
           }
         },
@@ -45,26 +36,5 @@ describe("user resolver", () => {
       console.log(res.data?.createUser);
       expect(res.data?.createUser).toHaveProperty("id");
     });
-
-    // it("should not create wilder given invalid attributes and return an error", async () => {
-    //   expect(() =>
-    //     client.mutate({
-    //       mutation: createWilderMutation,
-    //       variables: { data: { name: "" } },
-    //     })
-    //   ).rejects.toThrow();
-    // });
   });
-
-  // describe("read wilders", () => {
-  //   it("should return an array", async () => {
-  //     const res = await client.query({
-  //       query: readWildersQuery,
-  //       fetchPolicy: "no-cache",
-  //     });
-  //
-  //     expect(res.data.wilders[0]).toHaveProperty("id");
-  //     expect(res.data.wilders[0]).toHaveProperty("name");
-  //   });
-  // });
 });
