59e3381d5c26ffe2081ac3ad8e707d87fa4e0b17
Antoine <dumez.a@outlook.com>
2023-04-26 00:00:27 +0200

    feat: add ci/cd to main for production. fix: few modifications on statging

--- bin/sh/deploy-staging.sh
+++ bin/sh/deploy-staging.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cd .. && cd .. && git fetch origin && git reset --hard origin/develop && git clean -f -d && \
+cd ../.. && git fetch origin && git reset --hard origin/staging && git clean -f -d && \
 docker compose -f docker-compose.staging.yml down && \
 docker compose -f docker-compose.staging.yml pull && \
 docker compose -f docker-compose.staging.yml --env-file .env.staging up -d;
