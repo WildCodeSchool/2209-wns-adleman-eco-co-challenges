726b83cc561bad52a3de8b6c71e30e36107fe3f2
Antoine <dumez.a@outlook.com>
2023-04-28 15:51:52 +0200

    fix: delete error in deloy-prod

--- bin/sh/deploy-production.sh
+++ bin/sh/deploy-production.sh
@@ -6,11 +6,3 @@ git clean -f -d && \
 docker compose -f docker-compose.production.yml down && \
 docker compose -f docker-compose.production.yml pull && \
 docker compose -f docker-compose.production.yml --env-file .env.production up -d;
-
-cd ../.. && \
-git fetch origin && \
-git reset --hard origin/staging && \
-git clean -f -d && \
-docker compose -f docker-compose.staging.yml down && \
-docker compose -f docker-compose.staging.yml pull && \
-docker compose -f docker-compose.staging.yml --env-file .env.staging up -d;
