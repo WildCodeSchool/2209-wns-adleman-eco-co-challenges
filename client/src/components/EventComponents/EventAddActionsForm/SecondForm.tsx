60934c51e6acb386ab4a7a8f3f12f59dbf20742d
Antoine <dumez.a@outlook.com>
2023-05-26 00:15:36 +0200

    feat: finish the usestate filling, but not working with external fonction

--- client/src/components/EventComponents/EventAddActionsForm/SecondForm.tsx
+++ client/src/components/EventComponents/EventAddActionsForm/SecondForm.tsx
@@ -1,14 +1,38 @@
 import { useNavigate, useParams } from "react-router-dom";
+import {useState} from "react";
 
 const SecondForm = () => {
     const { id } = useParams();
     const navigate = useNavigate();
+
+    const [actionObjects, setActionObjects] = useState([
+        {
+            title: "",
+            description: "",
+            points: "",
+        },
+        {
+            title: "",
+            description: "",
+            points: "",
+        },
+        {
+            title: "",
+            description: "",
+            points: "",
+        }
+    ]);
+
     function saveAndNavigate() {
         // TODO: save actions in database, navigate to the event page
-        console.log("coucou");
+        // Créer 3 objets qui contiennent les infos des 3 actions
+        // Modifier l'event précédemment créé pour lui ajouter les 3 actions
+
+                console.log("coucou");
         // navigate(`/event/${id}`);
     }
 
+console.log(actionObjects);
     return (
         <>
             <h1> Ajout des actions</h1>
@@ -26,8 +50,18 @@ const SecondForm = () => {
                             type="text"
                             className="form-control rounded-3"
                             id="firstFloatingTitle"
-                            placeholder="name@example.com"
+                            placeholder="Titre de l'action"
                             required={true}
+                            onChange={(e) => {
+                                setActionObjects( actionObjects.map(
+                                    (actionObject, index) => {
+                                        if (index === 0) {
+                                            return { ...actionObject, title: e.target.value };
+                                        }
+                                        return actionObject;
+                                    }
+                                    ));
+                            }}
                        />
                         <label htmlFor="firstFloatingTitle">
                             Nom de l'action
@@ -38,8 +72,18 @@ const SecondForm = () => {
                         <input type="textaera"
                                className="form-control rounded-3"
                                id="firstfloatingDescription"
-                               placeholder="Password"
+                               placeholder="Description"
                                required={true}
+                               onChange={(e) => {
+                                   setActionObjects( actionObjects.map(
+                                       (actionObject, index) => {
+                                           if (index === 0) {
+                                               return { ...actionObject, description: e.target.value };
+                                           }
+                                           return actionObject;
+                                       }
+                                   ));
+                               }}
                        />
                         <label htmlFor="firstfloatingDescription">
                             Description de l'action
@@ -51,6 +95,17 @@ const SecondForm = () => {
                                id="firstFloatingPoints"
                                placeholder="xp"
                                required={true}
+                               onChange={(e) => {
+                                   // setUseState(0, "points", parseInt(e.target.value));
+                                   setActionObjects( actionObjects.map(
+                                       (actionObject, index) => {
+                                             if (index === 0) {
+                                                 return {...actionObject, points: e.target.value};
+                                             }
+                                           return actionObject;
+                                       }
+                                   ));
+                               }}
                         />
                         <label htmlFor="firstFloatingPoints">Points que rapporte l'action</label>
                     </div>
@@ -64,6 +119,16 @@ const SecondForm = () => {
                             id="secondFloatingTitle"
                             placeholder="name@example.com"
                             required={true}
+                            onChange={(e) => {
+                                setActionObjects( actionObjects.map(
+                                    (actionObject, index) => {
+                                        if (index === 1) {
+                                            return { ...actionObject, title: e.target.value };
+                                        }
+                                        return actionObject;
+                                    }
+                                ));
+                            }}
                         />
                         <label htmlFor="secondFloatingTitle">
                             Nom de l'action
@@ -76,6 +141,16 @@ const SecondForm = () => {
                                id="secondfloatingDescription"
                                placeholder="Password"
                                required={true}
+                               onChange={(e) => {
+                                   setActionObjects( actionObjects.map(
+                                       (actionObject, index) => {
+                                           if (index === 1) {
+                                               return { ...actionObject, description: e.target.value };
+                                           }
+                                           return actionObject;
+                                       }
+                                   ));
+                               }}
                         />
                         <label htmlFor="secondfloatingDescription">
                             Description de l'action
@@ -87,6 +162,17 @@ const SecondForm = () => {
                                id="secondFloatingPoints"
                                placeholder="xp"
                                required={true}
+                               onChange={(e) => {
+                                   // setUseState(0, "points", parseInt(e.target.value));
+                                   setActionObjects( actionObjects.map(
+                                       (actionObject, index) => {
+                                           if (index === 1) {
+                                               return {...actionObject, points: e.target.value};
+                                           }
+                                           return actionObject;
+                                       }
+                                   ));
+                               }}
                         />
                         <label htmlFor="secondFloatingPoints">Points que rapporte l'action</label>
                     </div>
@@ -100,6 +186,16 @@ const SecondForm = () => {
                             id="thirdFloatingTitle"
                             placeholder="name@example.com"
                             required={true}
+                            onChange={(e) => {
+                                setActionObjects( actionObjects.map(
+                                    (actionObject, index) => {
+                                        if (index === 2) {
+                                            return { ...actionObject, title: e.target.value };
+                                        }
+                                        return actionObject;
+                                    }
+                                ));
+                            }}
                         />
                         <label htmlFor="thirdFloatingTitle">
                             Nom de l'action
@@ -112,6 +208,16 @@ const SecondForm = () => {
                                id="thirdfloatingDescription"
                                placeholder="Password"
                                required={true}
+                               onChange={(e) => {
+                                   setActionObjects( actionObjects.map(
+                                       (actionObject, index) => {
+                                           if (index === 2) {
+                                               return { ...actionObject, description: e.target.value };
+                                           }
+                                           return actionObject;
+                                       }
+                                   ));
+                               }}
                         />
                         <label htmlFor="thirdfloatingDescription">
                             Description de l'action
@@ -123,6 +229,17 @@ const SecondForm = () => {
                                id="thirdFloatingPoints"
                                placeholder="xp"
                                required={true}
+                               onChange={(e) => {
+                                   // setUseState(0, "points", parseInt(e.target.value));
+                                   setActionObjects( actionObjects.map(
+                                       (actionObject, index) => {
+                                           if (index === 2) {
+                                               return {...actionObject, points: e.target.value};
+                                           }
+                                           return actionObject;
+                                       }
+                                   ));
+                               }}
                         />
                         <label htmlFor="thirdFloatingPoints">Points que rapporte l'action</label>
                     </div>
