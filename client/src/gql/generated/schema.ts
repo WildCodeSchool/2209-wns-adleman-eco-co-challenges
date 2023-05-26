cc45c7ab27e1920ea341420e9850de3af3f9a81b
Aslb <anne-sophie.leber@socgen.com>
2023-05-26 14:26:06 +0200

    fix conflicts

--- client/src/gql/generated/schema.ts
+++ client/src/gql/generated/schema.ts
@@ -569,7 +569,8 @@ export type GetUsersLazyQueryHookResult = ReturnType<typeof useGetUsersLazyQuery
 export type GetUsersQueryResult = Apollo.QueryResult<GetUsersQuery, GetUsersQueryVariables>;
 export const GetProfileDocument = gql`
     query GetProfile {
-  profile  id
+  profile {
+    id
     nickName
     friends {
       id
