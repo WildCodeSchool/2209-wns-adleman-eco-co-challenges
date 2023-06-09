49184527e622c4a852027c19d32ab6ab4e97be01
Aslb <anne-sophie.leber@socgen.com>
2023-06-09 17:28:08 +0200

    end of the week mini changes

--- client/src/components/EventComponents/EventList/EventList.tsx
+++ client/src/components/EventComponents/EventList/EventList.tsx
@@ -34,34 +34,40 @@ const EventList = (props: Props) => {
   return (
     <>
       <div className="album mt-5">
-        <p className="fs-2 mb-5 fw-bold text-center">Les Ecoco-challenges</p>
-        <div className="eventList-container">
-          <div
-            className={`row mx-auto ${isMobile ? "row-cols-1" : "row-cols-4"}`}
-          >
-            {events.getEvents?.map((event: any) => (
-              <div className="col mb-3" key={event.id}>
-                <div className="card  shadow-sm">
-                  <img
-                    className="bd-placeholder-img card-img-top"
-                    alt={event.title}
-                    width="100%"
-                    height="225"
-                    src={randomImageUrl()}
-                  />
-                  <div className="card-body">
-                    <p className="card-text">
-                      This is a wider card with supporting text below as a
-                      natural lead-in to additional content. This content is a
-                      little bit longer.
-                    </p>
-                    <div className="d-flex justify-content-between align-items-center">
-                      <small className="text-body-secondary">9 mins</small>
+        <h1 className="eventList__title text-center">
+          Les Ecoco-challenges en cours
+        </h1>
+        <div className="container  d-flex justify-content-center">
+          <div className="eventList-container">
+            <div
+              className={`row mx-auto ${
+                isMobile ? "row-cols-1" : "row-cols-4"
+              }`}
+            >
+              {events.getEvents?.map((event: any) => (
+                <div className="col mb-3" key={event.id}>
+                  <div className="card  shadow-sm">
+                    <img
+                      className="bd-placeholder-img card-img-top"
+                      alt={event.title}
+                      width="100%"
+                      height="225"
+                      src={randomImageUrl()}
+                    />
+                    <div className="card-body">
+                      <p className="card-text">
+                        This is a wider card with supporting text below as a
+                        natural lead-in to additional content. This content is a
+                        little bit longer.
+                      </p>
+                      <div className="d-flex justify-content-between align-items-center">
+                        <small className="text-body-secondary">9 mins</small>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
         </div>
       </div>
