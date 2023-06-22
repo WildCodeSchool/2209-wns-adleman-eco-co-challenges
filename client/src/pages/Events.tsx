07758dc080b7fe10aff04255b3ef236affe23a1a
Antoine <dumez.a@outlook.com>
2023-06-22 15:04:54 +0200

    feat: add card resizing + description text handeling + add badge on event curent user have

--- client/src/pages/Events.tsx
+++ client/src/pages/Events.tsx
@@ -26,15 +26,24 @@ export default function Events() {
 
   return (
     <>
-      <div>
-        <div className="header">
-          <Header />
-        </div>
-        <EventList events={events} onUserClick={navigateToEvent} />
-      </div>
+      {/* Nav bar */}
       <div className="header">
-        <Footer />
+        <Header />
       </div>
+      {/* Heroe section */}
+      <div className="text-center text-lg-start album pt-5 mt-5">
+        <h1 className="display-4 fw-bold lh-1 text-body-emphasis mb-3">
+            Liste des ecoco challenges
+        </h1>
+        <p className="col-lg-10 fs-4">
+            Les ecoco challenges sont des défis à relever pour améliorer votre impact écologique.
+            N'hésitez pas à vous inscrire à un ecoco challenge pour participer à la communauté et gagner des points !
+        </p>
+      </div>
+        {/* Event list */}
+      <EventList events={events} onUserClick={navigateToEvent} />
+      {/* Footer */}
+      <Footer />
     </>
   );
 }
