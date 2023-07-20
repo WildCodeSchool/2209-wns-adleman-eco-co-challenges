a04f455494aff917bcfd34887045304094cb66a1
Aslb <anne-sophie.leber@socgen.com>
2023-07-20 11:29:34 +0200

    fix loader

--- mobile-client/components/Loading/index.jsx
+++ mobile-client/components/Loading/index.jsx
@@ -1,11 +1,22 @@
-import { Layout, Spinner } from '@ui-kitten/components';
+import React from 'react';
+import { View, ActivityIndicator, StyleSheet } from 'react-native';
 
 const LoadingComponent = () => {
     return (
-        <Layout>
-            <Spinner size='giant' />
-        </Layout>
+        <View style={styles.container}>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
     );
 }
 
 export default LoadingComponent
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
